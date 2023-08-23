c9ad78b178042bd515e7449f07a15bd176379c6d
Kalu Ogba Kalu <kaluogba1990@gmail.com>
2023-08-23 06:52:59 -0700

    dynamic libraries

--- 0x18-dynamic_libraries/101-win.c
+++ 0x18-dynamic_libraries/101-win.c
@@ -1,6 +1,15 @@
+#define _GNU_SOURCE
+#include <dlfcn.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int rand() {
-    return 9; // Force winning number 9
+int rand(void) {
+    static int (*original_rand)(void) = NULL;
+
+    if (!original_rand) {
+        original_rand = dlsym(RTLD_NEXT, "rand");
+    }
+
+    // Force winning number 9
+    return 9;
 }
